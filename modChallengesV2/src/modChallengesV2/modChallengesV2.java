13d062feb5443bab3d97cd6bc823b79437ef1655
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-08-30 08:36:57 -0600

    completed the leap year challenge

--- modChallengesV2/src/modChallengesV2/modChallengesV2.java
+++ modChallengesV2/src/modChallengesV2/modChallengesV2.java
@@ -7,10 +7,17 @@ public class modChallengesV2
 			{
 				showModChallengeOne();
 			}
-		public static void showModChallengesOne()
+		public static void showModChallengeOne()
 			{
 				Scanner userInput = new Scanner(System.in);
+				System.out.println("please enter a year to find out if it's a leap year");
 				int leapYear = userInput.nextInt();
-				
+				if ((leapYear % 4 == 0) &&! (leapYear % 100 == 0 && leapYear % 400 != 0))
+					{
+						System.out.println("It's a leap year!");
+					}
+				else {
+					System.out.println("It's not a leap year!");
+				}
 			}
 	}
